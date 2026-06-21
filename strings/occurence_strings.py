d860c8f7c220405e513337b76894e4d147a65a85
sagarkarra <sagarkarra06@gmail.com>
2026-06-21 13:57:09 +0530

    Refactor character counting logic in occurence_strings.py
    Fix indentation and improve character counting function.

--- strings/occurence_strings.py
+++ strings/occurence_strings.py
@@ -8,7 +8,6 @@ Enter the char you want to count:a
 Occurecnce of characters: 2
 
 Find how many times 'a' occurs.
-'''
 s=input("Enter a string:")
 count=0
 str=input("Enter the char you want to count:")
@@ -16,6 +15,7 @@ for i in s:
     if i in str:
         count+=1
 print("Occurecnce of characters:",count)
+'''
 '''
 Solution:2
 '''

d504a786792b3161e0824fab4fd633c88a06229e
sagarkarra <sagarkarra06@gmail.com>
2026-06-27 16:25:55 +0530

    adding cmd_line_args.py codes

---
+++ cmd_line_args.py
@@ -0,0 +1,13 @@
+'''
+Input:
+Print Command-Line Arguments
+Output:
+The command line arguments are:
+c:/Users/JYOTH/Desktop/python_practice/cmd_line_args.py
+
+Print all command-line arguments passed to the script.
+'''
+import sys
+print("The command line arguments are:")
+for i in sys.argv:
+    print(i)

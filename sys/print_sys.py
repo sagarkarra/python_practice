c51f2b12060c5293e16a5d71428bf587e4e9f696
sagarkarra <sagarkarra06@gmail.com>
2026-06-27 16:21:10 +0530

    adding print_sys.py codes

---
+++ sys/print_sys.py
@@ -0,0 +1,12 @@
+'''
+Input:
+Print Python Version
+
+Output:
+The version of Python is 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)]
+
+Print the current Python version using the sys module
+'''
+import sys
+
+print("The version of Python is",sys.version)

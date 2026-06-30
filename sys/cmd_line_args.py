2b2b51498dbfa0f18f2785c1a1cbf766949f8e71
sagarkarra <sagarkarra06@gmail.com>
2026-06-30 16:21:59 +0530

    Rename function to print_arguments and update call

--- sys/cmd_line_args.py
+++ sys/cmd_line_args.py
@@ -15,9 +15,9 @@ for i in sys.argv:
 Solution 2:
 '''
 import sys
-def count_arguments():
+def print_arguments():
     print("The command line arguments are:")
     for i in sys.argv:
         print(i)
         
-count_arguments()
+print_arguments()

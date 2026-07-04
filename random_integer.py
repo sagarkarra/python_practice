fe475de13e7a846ff89212d18e32c4e55908f0c5
sagarkarra <sagarkarra06@gmail.com>
2026-07-04 16:26:41 +0530

    adding random_integer.py codes

---
+++ random_integer.py
@@ -0,0 +1,19 @@
+'''
+Input:
+Generate a random integer between 1 and 100.
+
+Output:
+Random integer blw 1 and 100 is 80
+
+import random
+print("Random integer blw 1 and 100 is",random.randint(1,100))
+
+Solution 2:
+'''
+import random
+def random_integer():
+    s=random.randint(1,100)
+    return s
+r=random_integer()
+print("Random integer b/w 1 and 100 is",r)
+

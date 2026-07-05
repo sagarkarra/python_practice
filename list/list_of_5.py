cf143d0c28e2fbce8e824d8bb738e2f1e26f11ad
sagarkarra <sagarkarra06@gmail.com>
2026-07-05 13:42:49 +0530

    adding list_of_5.py codes

---
+++ list/list_of_5.py
@@ -0,0 +1,16 @@
+'''
+Input:
+Create a list of 5 integers and print it.
+Output:
+List of 5 integers is [1, 2, 3, 4, 5]
+
+li=list(range(1,6))
+print("List of 5 integers is",li)
+
+Solution 2:
+'''
+def list_int():
+    l=list(range(1,6))
+    return l
+list_5=list_int()
+print("List of 5 integers is",list_5)

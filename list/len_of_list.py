93da95617eb188d0f0a644f11454a5c6c4f83a88
sagarkarra <sagarkarra06@gmail.com>
2026-07-05 13:56:41 +0530

    Move length calculation to len_list function
    Refactor length calculation into a separate function.

--- list/len_of_list.py
+++ list/len_of_list.py
@@ -1,7 +1,18 @@
 '''
 Input:
 Find the length of a list without hardcoding the answer.
-'''
+Ouptut:
+Enter values:10 20 30 40 50
+Length of list: 5
+
 s=input("Enter values:").split()
 length=len(s)
 print("Length of list:",length)
+'''
+
+def len_list():
+    length=len(s)
+    return length
+s=input("Enter values:").split()
+result=len_list()
+print("Length of list:",result)

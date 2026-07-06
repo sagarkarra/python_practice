8c24ebef5a568cc62434a94e211be520bf05752f
sagarkarra <sagarkarra06@gmail.com>
2026-07-06 13:49:50 +0530

    adding element at first.py codes

---
+++ list/insert_element_at_first.py
@@ -0,0 +1,22 @@
+'''
+Input:
+Insert an element at the beginning of a list.
+Output:
+Enter string:theresa sagar
+Enter element to insert:jyothsna
+After inserting element at first ['jyothsna', 'theresa', 'sagar']
+
+s=input("Enter string:").split()
+d=input("Enter element to insert:")
+s.insert(0,d)
+print("After inserting element at first",s)
+
+Solution 2:
+'''
+def insert_element():
+    s.insert(0,d)
+    return s
+s=input("Enter string:").split()
+d=input("Enter element to insert:")
+res=insert_element()
+print("After inserting element at first",res)

98d2febb8f7d0e83f5bb15322e6be0f8977d8780
sagarkarra <sagarkarra06@gmail.com>
2026-07-09 16:43:29 +0530

    adding copy list without affecting original.py codes

--- list/copy_list_original.py
+++ list/copy_list_original.py
@@ -2,25 +2,27 @@
 Input:
 Copy a list without affecting the original.
 Output:
-Enter strings:1 2 3
+Enter strings:1 2 3 
 Enter element to s:4
 Output of s is ['1', '2', '3']
 Output of r is ['1', '2', '3', '4']
 
 s=input("Enter strings:").split()
-w=s.copy()
+w=s.copy() 
 r=input("Enter element to s:")
 w.append(r)
-print("Output of s is",s)
-print("Output of r is",w)
+print("Output of s is",s) 
+print("Output of r is",w) 
 
 Solution 2:
 '''
+
 def copy_list():
-    w=s.copy()
+    w = s.copy()
     w.append(r)
-    print("Output of s is",s)
+    print("Output of s is:",s)
     print("Output of r is",w)
-s=input("Enter strings:").split()
-r=input("Enter element to s:")
+s = input("Enter strings:").split()
+r = input("Enter element to s:")
 copy_list()
+

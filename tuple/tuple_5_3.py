fabe0ed732d0a205f087631a0c0877d63fe73711
sagarkarra <sagarkarra06@gmail.com>
2026-07-10 17:01:31 +0530

    adding tuple of 5 elements.py codes

---
+++ tuple/tuple_5_3.py
@@ -0,0 +1,16 @@
+'''
+Input:
+Create a tuple of five integers and print the third element.
+Output:
+Enter 5 elements:1 2 3 4 5
+3
+
+t=input("Enter 5 elements:").split()
+print(t[2])
+
+Solution 2:
+'''
+def tuple_5():
+    t=input("Enter 5 elements:").split()
+    print(t[2])
+tuple_5()

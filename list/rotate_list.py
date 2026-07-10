cf1708bde17476d9f59be7f3ba147761ba7ced36
sagarkarra <sagarkarra06@gmail.com>
2026-07-10 13:27:00 +0530

    Refactor list rotation to use rotate_list function
    Refactor list rotation logic into a separate function.

--- list/rotate_list.py
+++ list/rotate_list.py
@@ -4,9 +4,20 @@ Rotate a list by one position to the right.
 Output:
 Enter the elements: 1 2 3 4 5
 Rotated list: ['5', '1', '2', '3', '4']
-'''
+
 s = input("Enter the elements: ").split()
 if s:
     s.insert(0, s.pop())
 
 print("Rotated list:", s)
+
+Solution 2:
+'''
+def rotate_list(s):
+    if s:
+        s.insert(0, s.pop())
+    return s
+
+s = input("Enter the elements: ").split()
+
+print("Rotated list:", rotate_list(s))

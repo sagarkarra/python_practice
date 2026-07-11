966cce2cda457c7a412661227cf8c15428a24628
sagarkarra <sagarkarra06@gmail.com>
2026-07-11 15:54:13 +0530

    adding tule index.py codes

---
+++ tuple/tuple_index.py
@@ -0,0 +1,24 @@
+'''
+Input:
+Find the index of a specified element in a tuple.
+Output:
+Enter elements:1 2 3 4 5
+Enter the element to find:3
+('1', '2', '3', '4', '5')
+Index of element is: 2
+
+s=tuple(input("Enter elements:").split())
+t=input("Enter the element to find:")
+u=s.index(t)
+print(s)
+print("Index of element is:",u)
+
+Solution 2:
+'''
+def tuple_index():
+    print(s)
+    u=s.index(t)
+    print("Index of element is:",u)
+s=tuple(input("Enter elements:").split())
+t=input("Enter the element to find:")
+tuple_index()

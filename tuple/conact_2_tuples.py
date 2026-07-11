e56d8236c19f3708c687a1575c847e1e8b5f9962
sagarkarra <sagarkarra06@gmail.com>
2026-07-11 13:40:46 +0530

    adding count of value using tuple.py codes

--- tuple/conact_2_tuples.py
+++ tuple/conact_2_tuples.py
@@ -1,4 +1,21 @@
 '''
 Input:
 Concatenate two tuples and display the result.
+Output:
+Enter values of t1:10 20 30
+Enter values of t2:40 50 60
+Final tuple is: ['10', '20', '30', '40', '50', '60']
+
+t1=input("Enter values of t1:").split()
+t2=input("Enter values of t2:").split()
+t3=t1+t2
+print("Final tuple is:",t3)
+
+Solution 2:
 '''
+def concat_2_tuples():
+    t3=t1+t2
+    print("Final tuple is:",t3)
+t1=input("Enter values of t1:").split()
+t2=input("Enter values of t2:").split()
+concat_2_tuples()
---
+++ tuple/count_tuple.py
@@ -0,0 +1,21 @@
+'''
+Input:
+Count how many times a given value appears in a tuple.
+Output:
+Enter values of t1:1 2 2 3 3
+Enter element to count:1
+Count of value is: 1
+
+t1=input("Enter values of t1:").split()
+s=input("Enter element to count:")
+w=t1.count(s)
+print("Count of value is:",w)
+
+Solution 2:
+'''
+def count_tuple():
+    w=t1.count(s)
+    print("Count of value is:",w)
+t1=input("Enter values of t1:").split()
+s=input("Enter element to count:")
+count_tuple()

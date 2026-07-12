8edbdc9debf11c36a3ab0a79c0c55c85fc820718
sagarkarra <sagarkarra06@gmail.com>
2026-07-12 16:57:35 +0530

    Refactor to use a function for tuple of squares

--- tuple/tuple_of_squares.py
+++ tuple/tuple_of_squares.py
@@ -5,9 +5,19 @@ Output:
 Enter a number:5
 Square of Tuple values from 1 to 5:
 (1, 4, 9, 16, 25)
-'''
+
 n=int(input("Enter a number:"))
 y=[x*x for x in range(1,n+1)]
 z=tuple(y)
 print(f"Square of Tuple values from 1 to {n}:")
 print(z)
+
+Solution 2:
+'''
+def tuple_of_squares():
+    y=[x*x for x in range(1,n+1)]
+    z=tuple(y)
+    print(f"Square of Tuple values from 1 to {n}:")
+    print(z)
+n=int(input("Enter a number:"))
+tuple_of_squares()

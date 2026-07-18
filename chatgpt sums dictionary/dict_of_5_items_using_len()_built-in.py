569e951c5ad1bfecd6983c72527031672b99b199
sagarkarra <sagarkarra06@gmail.com>
2026-07-18 16:05:04 +0530

    adding len() built-in function.py codes

---
+++ chatgpt sums dictionary/dict_of_5_items_using_len()_built-in.py
@@ -0,0 +1,15 @@
+'''
+Input:
+Create a dictionary with five items and print its length.
+Output:
+Length of 5 items is:5
+
+d1={"Name":"Sagar","Age":20,"Place":"Gutur","Study":"B.Tech","College":"VVITU"}
+print("Length of 5 items is:%d"%len(d1))
+
+Solution 2:
+'''
+def dict_5_items():
+    print("Length of 5 items is:%d"%len(d1))
+d1={"Name":"Sagar","Age":20,"Place":"Gutur","Study":"B.Tech","College":"VVITU"}
+dict_5_items()

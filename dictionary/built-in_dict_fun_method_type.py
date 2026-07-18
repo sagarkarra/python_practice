67ed4080991ea61c201a2358809dea2e4f19cd26
sagarkarra <sagarkarra06@gmail.com>
2026-07-18 15:55:47 +0530

    adding bulit-in function type_function.py codes

---
+++ dictionary/built-in_dict_fun_method_type.py
@@ -0,0 +1,16 @@
+'''
+Input:
+Write a program to print the Variable type of a dictionary.
+Output:
+Variable Type:<class 'dict'>
+
+d1={"Name":"Sagar","Age":20,"Class":"First"}
+print("Variable Type:%s" %type(d1))
+
+Solution 2:
+'''
+def type_fun():
+    print("Variable Type:%s" %type(d1))
+d1={"Name":"Sagar","Age":20,"Class":"First"}
+type_fun()
+

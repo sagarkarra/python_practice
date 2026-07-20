74a4fd676c5f8eebe6b9a74305fda82daea01f39
sagarkarra <sagarkarra06@gmail.com>
2026-07-20 13:45:28 +0530

    adding popitem_que.py codes

---
+++ chatgpt sums dictionary/pop_item_sum.py
@@ -0,0 +1,16 @@
+'''
+Input:
+Removes the last inserted key-value pair.
+Output:
+('c',30)
+{'a':10,'b':20}
+
+d = {"a":10,"b":20,"c":30}
+print(d.popitem())
+print(d)
+'''
+def popitem_sum():
+    print(d.popitem())
+    print(d)
+d = {"a":10,"b":20,"c":30}
+popitem_sum()

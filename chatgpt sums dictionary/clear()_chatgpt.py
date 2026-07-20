735b6cb98dbe63646fc023e15066461d2456609c
sagarkarra <sagarkarra06@gmail.com>
2026-07-20 12:33:40 +0530

    adding clear() chatgpt question.py codes

---
+++ chatgpt sums dictionary/clear()_chatgpt.py
@@ -0,0 +1,20 @@
+'''
+Input:
+Removes all elements.
+Output:
+{'Name': 'Vani', 'Age': 22}
+{}
+
+d = {"Name":"Vani","Age":22}
+print(d)
+d.clear()
+print(d)
+
+Solution 2:
+'''
+def clear():
+    print(d)
+    d.clear()
+    print(d)
+d = {"Name":"Vani","Age":22}
+clear()

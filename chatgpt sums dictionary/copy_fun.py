f826f3b385a093c3183359007e829fd8dbcd8a1d
sagarkarra <sagarkarra06@gmail.com>
2026-07-20 13:49:33 +0530

    adding copy_fun_chatgpt.py codes

---
+++ chatgpt sums dictionary/copy_fun.py
@@ -0,0 +1,17 @@
+'''
+Input:
+Creates a shallow copy.
+Output:
+New dictionary: {'Name': 'Manvi', 'Age': 7}
+
+d1 = {"Name":"Manvi","Age":7}
+d2 = d1.copy()
+print("New dictionary:",d2)
+
+Solution 2:
+'''
+def copy_fun_chatgpt():
+    d2 = d1.copy()
+    print("New dictionary:",d2)
+d1 = {"Name":"Manvi","Age":7}
+copy_fun_chatgpt()

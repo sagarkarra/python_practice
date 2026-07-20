dfdf065ab2d34edb2cdb5c854cb7ce79a0fd1b43
sagarkarra <sagarkarra06@gmail.com>
2026-07-20 13:04:40 +0530

    adding pop() chatgpt question.py codes

---
+++ chatgpt sums dictionary/pop()_chatgpt.py
@@ -0,0 +1,23 @@
+'''
+Input:
+Remove specified key.
+d.pop(30)
+If key does not exist:
+d.pop(100)
+Output:
+{10: 1.2, 20: 3.4, 40: 4.5}
+d.pop(100)   KeyError: 100
+
+d = {10:1.2,20:3.4,30:1.2,40:4.5}
+d.pop(30)
+print(d)
+d.pop(100)
+
+Solution 2: 
+'''
+def pop_chatgpt():
+    d.pop(30)
+    print(d)
+    d.pop(100)
+d = {10:1.2,20:3.4,30:1.2,40:4.5}
+pop_chatgpt()

fd295a1990ae725dd65b11e57f081d6bef89ce80
sagarkarra <sagarkarra06@gmail.com>
2026-07-23 13:55:38 +0530

    adding dict_update.py codes

---
+++ dictionary/dict_update.py
@@ -0,0 +1,19 @@
+'''
+Input:
+add 2 dictionary 2 key-value pairs
+Output:
+Updated dict: {'Name': 'Vani', 'Age': 7, 'Sex': 'Female'}
+
+d1={'Name':'Vani','Age':7}
+d2={'Sex':'Female'}
+d1.update(d2)
+print("Updated dict:",d1)
+
+Solution 2:
+'''
+def dict_update():
+    d1.update(d2)
+    print("Updated dict:",d1)
+d1={'Name':'Vani','Age':7}
+d2={'Sex':'Female'} 
+dict_update

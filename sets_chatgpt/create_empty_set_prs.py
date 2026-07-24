9c1e948039221acfa2203f1a08b0726316d43464
sagarkarra <sagarkarra06@gmail.com>
2026-07-24 13:35:32 +0530

    adding empty_set and list_set_prs.py codes

---
+++ sets_chatgpt/create_empty_set_prs.py
@@ -0,0 +1,36 @@
+'''
+Input:
+Create an empty set and print its type.
+Create a set with the elements:
+10, 20, 30, 40, 50
+Output:
+set()
+<class 'set'>
+{40, 10, 50, 20, 30}
+<class 'set'>
+
+s=set()
+print(s)
+print(type(s))
+s.add(10)
+s.add(20)
+s.add(30)
+s.add(40)
+s.add(50)
+print(s)
+print(type(s))
+
+Solution 2:
+'''
+def create_empty_set_prs():
+    print(s)
+    print(type(s))
+    s.add(10)
+    s.add(20)
+    s.add(30)
+    s.add(40)
+    s.add(50)
+    print(s)
+    print(type(s))
+s=set()
+create_empty_set_prs()
---
+++ sets_chatgpt/using_list_set_prs.py
@@ -0,0 +1,19 @@
+'''
+Input:
+Create a set using the set() function from the list:
+[1, 2, 3, 4, 5]
+Output:
+{1, 2, 3, 4, 5}
+<class 'set'>
+
+se1=set([1, 2, 3, 4, 5])
+print(se1)
+print(type(se1))
+
+Solution 2:
+'''
+def using_list_set_prs():
+    print(se1)
+    print(type(se1))
+se1=set([1, 2, 3, 4, 5])
+using_list_set_prs()

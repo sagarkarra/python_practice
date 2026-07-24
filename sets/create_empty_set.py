8f7ce95b54bfcb60b153fd930a0823e85402aec9
sagarkarra <sagarkarra06@gmail.com>
2026-07-24 13:01:46 +0530

    adding creating empty set and adding values and using set_list.py codes

---
+++ sets/create_empty_set.py
@@ -0,0 +1,26 @@
+'''
+Input:
+Write a program to create an empty set using set() and print it and add elements to the empty set
+Output:
+set()
+{10, 20, 30}
+
+s1=set()
+print(s1)
+s1.add(10)
+s1.add(20)
+s1.add(30)
+s1.add(10)
+print(s1)
+
+Solution 2:
+'''
+def create_set():
+    print(s1)
+    s1.add(10)
+    s1.add(20)
+    s1.add(30)
+    s1.add(10)
+    print(s1)
+s1=set()
+create_set()
---
+++ sets/using_list_set.py
@@ -0,0 +1,18 @@
+'''
+Input:
+Write a program to create a set inside a list and print the set
+Output:
+{1, 2, 4, 'a', (2+4j)}
+<class 'set'>
+
+s2=set([1,2,4,'a',(2+4j),True])
+print(s2)
+print(type(s2))
+
+Solution 2:
+'''
+def using_list_set():
+    print(s2)
+    print(type(s2))
+s2=set([1,2,4,'a',(2+4j),True])
+using_list_set()

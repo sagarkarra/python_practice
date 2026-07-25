efa55cf53ff31d012658822c61a7001b96ead253
sagarkarra <sagarkarra06@gmail.com>
2026-07-25 16:35:02 +0530

    adding set.py codes

---
+++ sets/set.py
@@ -0,0 +1,26 @@
+'''
+Input:
+Write a program to create a set by using a sequence
+Output:
+{'o', 'c', 'w', 'l', 'm', 'e'}
+<class 'set'>
+example 2 output:
+{'Java', 'perl', 'python'}
+
+a=set("welcome")
+print(a)
+print(type(a))
+print()
+b=set(["perl","python","Java"])
+print(b)
+
+Solution 2:
+'''
+def set():
+    a=set("welcome")
+    print(a)
+    print(type(a))
+    print()
+    b=set(["perl","python","Java"])
+    print(b)
+set()

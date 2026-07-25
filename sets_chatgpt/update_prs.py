8692755066a0a880d883dbd50c8376d805332005
sagarkarra <sagarkarra06@gmail.com>
2026-07-25 16:38:48 +0530

    adding update_prs.py codes

---
+++ sets_chatgpt/update_prs.py
@@ -0,0 +1,31 @@
+'''
+Input:
+we can't do
+Output:
+{20, 5, 40, 10, 30}
+<class 'set'>
+2381863676928
+
+se={10,20,30,40,30,5}
+print(se)
+print(type(se))
+print(id(se))
+print(se[0])
+print()
+fruits={'apple','orange','apple','pear','orange','banana'}
+print(fruits)
+print('orange' in fruits)
+
+Solution 2:
+'''
+def update_prs():
+    se={10,20,30,40,30,5}
+    print(se)
+    print(type(se))
+    print(id(se))
+    print(se[0])
+    print()
+    fruits={'apple','orange','apple','pear','orange','banana'}
+    print(fruits)
+    print('orange' in fruits)
+update_prs()

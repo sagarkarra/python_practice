223d6d43d0899aac0454786a451fa49013f9e520
sagarkarra <sagarkarra06@gmail.com>
2026-07-26 14:13:44 +0530

    adding copy_set.py codes

---
+++ sets/copy_set.py
@@ -0,0 +1,29 @@
+'''
+Input:
+Write a program to create a copy of set elements and return it
+Output:
+{'a', 'g', 'f', 'h', 'e', 'd', 'c', 'b'}
+
+x=set('abcdefgh')
+y=set('abcd')
+x.copy()
+print(x)
+'''
+
+'''
+Another method of copy
+y=x.copy()
+print(y)
+x=y.copy()
+print(x)
+'''
+
+'''
+Solution 2:
+'''
+def copy_set():
+    x.copy()
+    print(x)
+x=set('abcdefgh')
+y=set('abcd')
+copy_set()

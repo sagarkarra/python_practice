048a452d91e3e3d95eeaf86adff1e2c0c8a3ee1e
sagarkarra <sagarkarra06@gmail.com>
2026-07-26 14:29:18 +0530

    adding copy_set_prs.py codes

---
+++ sets_chatgpt/copy_set_prs.py
@@ -0,0 +1,29 @@
+'''
+Input:
+Copy the set:{"red", "green", "blue"} into another variable.
+Add "yellow" to the copied set.
+Check whether the original set changes.
+
+s={"red", "green", "blue"}
+x=s.copy()
+x.add("yellow")
+print(s)
+print(x)
+if x == s:
+    print("Set changed")
+else:
+    print("Set not changed")
+
+Solution 2:
+'''
+def copy_set_prs():
+    x=s.copy()
+    x.add("yellow")
+    print(s)
+    print(x)
+    if x == s:
+        print("Set changed")
+    else:
+        print("Set not changed")
+s={"red", "green", "blue"}
+copy_set_prs()
